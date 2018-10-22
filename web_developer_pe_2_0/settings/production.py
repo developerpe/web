9841831a9b3c10267c5fc7d0bda1507b82dacf11
elunicorom <barsa_real_31@hotmail.com>
2018-10-21 23:47:49 -0500

    allowed host corregido

--- web_developer_pe_2_0/settings/production.py
+++ web_developer_pe_2_0/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['developerpe.herokuapp.com']
+ALLOWED_HOSTS = ['developerpe.herokuapp.com','www.developerpe.com']
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
