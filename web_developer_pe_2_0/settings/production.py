b801e4d615d504993cf78b75d75ab0426e447850
elunicorom <barsa_real_31@hotmail.com>
2018-10-21 20:22:14 -0500

    static url corregido

--- web_developer_pe_2_0/settings/production.py
+++ web_developer_pe_2_0/settings/production.py
@@ -27,3 +27,28 @@ DATABASES = {
 }
 
 """
+
+JET_DEFAULT_THEME = 'light-violet'
+JET_SIDE_MENU_COMPACT = True
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+MEDIA_URL = '/media/'
+STATICFILES_DIRS=[
+	os.path.join(os.path.dirname(BASE_DIR),'static'),
+		]
+
+AUTH_USER_MODEL = 'usuarios.User'
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
+
+
+
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': None,
+    },
+}
