2f8370369ddff14cb77d6758c0128b27b533bc9b
elunicorom <barsa_real_31@hotmail.com>
2018-10-21 20:18:21 -0500

    credenciales de heroku añadidas

--- web_developer_pe_2_0/settings/production.py
+++ web_developer_pe_2_0/settings/production.py
@@ -2,17 +2,17 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['developerpe.herokuapp.com']
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'developer',
-        'USER': 'oliver',
-        'PASSWORD': 'yugiho2000',
-        'HOST': 'localhost',
+        'NAME': 'd25i063gce88cj',
+        'USER': 'hemvrjpefiaflv',
+        'PASSWORD': '6d45ea769ca2ca6f9bf14eed3129a16f97d21c7ea57b42308d894e181eb52286',
+        'HOST': 'ec2-23-21-147-71.compute-1.amazonaws.com',
         'PORT': 5432
     }
 }
--- web_developer_pe_2_0/wsgi.py
+++ web_developer_pe_2_0/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_developer_pe_2_0.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_developer_pe_2_0.settings.production")
 
 application = get_wsgi_application()
