da573c449b256fe1270cea6add1273a64a90c973
elunicorom <barsa_real_31@hotmail.com>
2018-10-26 01:07:36 -0500

    models

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -52,7 +52,7 @@ class Autor(models.Model):
 
 class Post(models.Model):
     id = models.AutoField(primary_key = True)
-    titulo = models.CharField('Título', max_length = 40, blank = False, null = False)
+    titulo = models.CharField('Título', max_length = 70, blank = False, null = False)
     slug = models.CharField('Slug', max_length = 255, blank = False, null = False)
     description = models.CharField('Descripción',max_length = 50, null = False, blank = False)
     imagen_previa = models.URLField(max_length = 255, null=False,blank=False)
