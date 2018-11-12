3fefc19c7d3bb6f2f61b1109982b04067dca4332
elunicorom <barsa_real_31@hotmail.com>
2018-11-11 23:32:08 -0500

    popup de suscripcion agregada

---
+++ static/blog/js/suscription.js
@@ -0,0 +1,33 @@
+'use strict';
+$(document).ready(function(){
+  $('#Suscripcion').addClass("is-active");
+});
+$(".delete").click(function() {
+   $("#Suscripcion").removeClass("is-active");
+});
+$("#cerrar").click(function() {
+   $("#Suscripcion").removeClass("is-active");
+});
+$('#subscribe').click(function(){
+    $('#message').fadeIn(5000,function(){
+        document.getElementById('message').innerHTML = 'Enviando...';
+    });
+});
+$('#mc-form').submit(function(event){
+    event.preventDefault();
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        data: $('#mc-form').serialize(),
+        success: function(response){
+          console.log(response);
+            $('#message').fadeIn(5000,function(){
+                document.getElementById('message').innerHTML = 'Su registro fué completado correctamente!\nRecuerda revisar en tu bandeja de No deseados! :D';
+                document.getElementById('mc-email').value = '';
+            });
+            setTimeout(function(){
+              $("#Suscripcion").removeClass("is-active");
+            },1000);
+          }
+    });
+});
